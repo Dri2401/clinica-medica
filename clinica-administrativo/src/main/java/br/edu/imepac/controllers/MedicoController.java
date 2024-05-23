ff49e9517d10aced0bd4ee6b119b553b1766f81e
Italo204 <italoosantos32@gmail.com>
2024-05-22 22:29:42 -0300

    MedicoModel feito

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/MedicoController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/MedicoController.java
@@ -1,5 +1,8 @@
 package br.edu.imepac.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MedicoController {
 
 }
--- clinica-administrativo/src/main/java/br/edu/imepac/repositories/MedicoRepository.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/repositories/MedicoRepository.java
@@ -1,5 +1,10 @@
 package br.edu.imepac.repositories;
 
-public interface MedicoRepository {
+import br.edu.imepac.models.MedicoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<MedicoModel, Long> {
 
 }
