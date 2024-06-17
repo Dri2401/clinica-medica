bf343143978439d57fc6325bfc48f00bf3d42e0d
Italo204 <italoosantos32@gmail.com>
2024-06-17 15:55:38 -0300

    Convenio e Especialidade Controller Documentados com Swagger!

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/ConvenioController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/ConvenioController.java
@@ -3,6 +3,11 @@ package br.edu.imepac.controllers;
 import br.edu.imepac.dtos.Convenio.ConvenioDtoRequest;
 import br.edu.imepac.dtos.Convenio.ConvenioDtoResponse;
 import br.edu.imepac.services.ConvenioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +20,19 @@ import java.util.List;
 
 @Controller
 @RequestMapping("convenio")
+@Tag(name = "Convenio-API")
 public class ConvenioController {
     @Autowired
     private ConvenioService convenioService;
 
     private static final Logger logger = LoggerFactory.getLogger(ConvenioController.class);
 
+    @Operation(summary = "Realiza o cadastro de convênios", method = "POST")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Cadastro do convênio realizada com sucesso!"),
+        @ApiResponse(responseCode = "400", description = "Erro na request feita pelo usuário"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @PostMapping
     public ResponseEntity<ConvenioDtoResponse> saveConvenio(@RequestBody ConvenioDtoRequest convenioDtoRequest) {
         logger.info("Request ConvenioCreateRequest ");
@@ -28,6 +40,12 @@ public class ConvenioController {
         return new ResponseEntity<>(saveConvenio, HttpStatus.CREATED);
     }
 
+    
+    @Operation(summary = "Retorna todos os convênios cadastrados!", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Request feita com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @GetMapping
     public ResponseEntity<List<ConvenioDtoResponse>> getAllConvenio() {
         logger.info("Request ConvenioGetAllRequest ");
@@ -35,6 +53,12 @@ public class ConvenioController {
         return new ResponseEntity<>(convenios, HttpStatus.OK);
     }
 
+    @Operation(summary = "Retorna o dado de um convênio pelo id", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Request feita com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Convênio não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ConvenioDtoResponse> getConvenioById(@PathVariable("id") Long id) {
@@ -47,6 +71,12 @@ public class ConvenioController {
         }
     }
 
+    @Operation(summary = "Atualiza um convênio", method = "PUT")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Convênio atualizado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Convênio não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ConvenioDtoResponse> updateConvenio(@PathVariable("id") Long id, @RequestBody ConvenioDtoResponse convenioDtoResponse) {
@@ -58,6 +88,12 @@ public class ConvenioController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+
+    @Operation(summary = "Exclusão de um convênio", method = "DELETE")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Convênio deletado com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> deleteConvenio(@PathVariable("id") Long id) {
--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/EspecialidadeController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/EspecialidadeController.java
@@ -4,6 +4,11 @@ package br.edu.imepac.controllers;
 import br.edu.imepac.dtos.Especialidade.EspecialidadeDtoRequest;
 import br.edu.imepac.dtos.Especialidade.EspecialidadeDtoResponse;
 import br.edu.imepac.services.EspecialidadeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +21,19 @@ import java.util.List;
 
 @Controller
 @RequestMapping("especialidade")
+@Tag(name = "Especialidade-API")
 public class EspecialidadeController {
     @Autowired
     private EspecialidadeService especialidadeService;
 
     private static final Logger logger = LoggerFactory.getLogger(EspecialidadeController.class);
 
-
+    @Operation(summary = "Realiza o cadastro de Especialidades", method = "POST")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Cadastro da especialidade realizada com sucesso!"),
+        @ApiResponse(responseCode = "400", description = "Erro na request feita pelo usuário"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @PostMapping
     public ResponseEntity<EspecialidadeDtoResponse> saveEspecialidade(@RequestBody EspecialidadeDtoRequest especialidadeDtoRequest) {
         logger.info("Request EspecialidadeCrateRequest ");
@@ -30,6 +41,11 @@ public class EspecialidadeController {
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Retorna todos as especialidades cadastradas!", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Request feita com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @GetMapping
     public ResponseEntity<List<EspecialidadeDtoResponse>> listAllEspecialidades() {
         logger.info("Request listAllEspecialidades ");
@@ -37,6 +53,12 @@ public class EspecialidadeController {
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
+    @Operation(summary = "Retorna o dado de uma especialidade pelo id", method = "GET")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Request feita com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Especialidade não encontrada"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<EspecialidadeDtoResponse> getEspecialidadeById(@PathVariable("id") Long id) {
@@ -48,6 +70,13 @@ public class EspecialidadeController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+
+    @Operation(summary = "Atualiza uma especialidade", method = "PUT")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Especialidade atualizada com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Especialidade não encontrado"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<EspecialidadeDtoResponse> updateEspecialidade(@PathVariable("id") Long id, @RequestBody EspecialidadeDtoResponse especialidadeDtoResponse) {
@@ -59,6 +88,11 @@ public class EspecialidadeController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Exclusão de uma especialidade", method = "DELETE")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Especialidade deletada com sucesso"),
+        @ApiResponse(responseCode = "500", description = "Erro interno")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> deleteEspecialidade(@PathVariable("id") Long id) {
